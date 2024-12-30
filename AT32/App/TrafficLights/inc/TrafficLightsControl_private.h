ee853a6fc6f08e8a04b03258f02a14fbe788bd23
esmail <esmailqassem@gmail.com>
2024-12-30 05:47:09 +0200

    Task 1

--- AT32/App/TrafficLights/inc/TrafficLightsControl_private.h
+++ AT32/App/TrafficLights/inc/TrafficLightsControl_private.h
@@ -19,4 +19,22 @@ typedef enum
 #define GREEN_DURATION  5
 #define YELLOW_DURATION 5
 #define RED_DURATION    5
+
+static void HandleGreenState(void)
+{
+	 DIO_SetPinVal(DIO_PORTC, DIO_Pin0, DIO_LOW);
+    DIO_SetPinVal(DIO_PORTC, DIO_Pin2, DIO_HIGH);
+}
+static void HandleRedState(void)
+{
+	DIO_SetPinVal(DIO_PORTC, DIO_Pin2, DIO_LOW);
+	DIO_SetPinVal(DIO_PORTC, DIO_Pin1, DIO_LOW);
+	DIO_SetPinVal(DIO_PORTC, DIO_Pin0, DIO_HIGH);
+}
+static void HandleyellowState(void)
+{
+	DIO_SetPinVal(DIO_PORTC, DIO_Pin2, DIO_LOW);
+	DIO_SetPinVal(DIO_PORTC, DIO_Pin0, DIO_LOW);
+    DIO_SetPinVal(DIO_PORTC, DIO_Pin1, DIO_HIGH);
+}
 #endif /* APP_TRAFFICLIGHTS_INC_TRAFFICLIGHTSCONTROL_PRIVATE_H_ */
